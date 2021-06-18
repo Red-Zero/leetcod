0a381940b3fb630f27a52465bdf2a8265017c12f
wulingnan <wulingnan@zhiyuanhui.com>
2021-06-18 09:17:52 +0800

    AC 18

---
+++ 剑指offer/18-删除链表的节点.js
@@ -0,0 +1,50 @@
+/* 
+给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+返回删除后的链表的头节点。
+注意：此题对比原题有改动
+示例 1:
+输入: head = [4,5,1,9], val = 5
+输出: [4,1,9]
+解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+示例 2:
+输入: head = [4,5,1,9], val = 1
+输出: [4,5,9]
+解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+说明：
+题目保证链表中节点的值互不相同
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+----------------------------------------------------------------------------
+题解:链表基本操作，如果head.val === val 直接返回head.next
+否则，任意节点node，如果node.next.val === val 则，node.next = node.next.next
+node初始值为head
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+ var deleteNode = function(head, val) {
+    if(head.val === val) return head.next
+    let node = head
+    while(node){
+        if(node.next.val === val){
+            node.next = node.next.next
+            return head
+        }else
+        {
+            node=node.next
+        }
+    }
+  return head
+  };
